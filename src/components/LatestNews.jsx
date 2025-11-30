60af24e34881a37e140a97956bd16b06c57681ad
Asad Zaman <10asadzaman@gmail.com>
2025-11-30 22:05:43 +0600

    design header, navbar, marquee section

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -6,7 +6,7 @@ const LatestNews = () => {
         <div className='flex items-center gap-5 bg-base-200 p-3 w-10/12 mx-auto'>
             <p className='text-white bg-secondary p-3 w-[110px] text-center'>Latest</p>
             <Marquee pauseOnClick={true}>
-                <p className='font-bold'>dolor sit amet, consectetur adipisicing elit. Perspiciatis quas similique quidem modi ut minima atque mollitia dicta commodi ss.</p>
+                <p className='font-bold'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, soluta..</p>
             </Marquee>
         </div>
     );
